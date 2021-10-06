d343ba39b1ae0b7ea8a1daaaae88899563d5207f
Ammar Farhan Mohamad Rizam <ammarfmr11@gmail.com>
2021-10-07 06:42:00 +0800

    Convert to OOP
    Convert to OOP to:
     - make future implementation of features easier
     - make it uniform to the style used for GUI
     - make it easier to work with when implementing functionalities in the GUI
    
     Add CLI options to:
     - provide different ways of providing custom values to the app
     - learn more about argparse in Python :P

--- generate_borang_ro.py
+++ generate_borang_ro.py
@@ -15,6 +15,8 @@ __emails__ = ["ammarfmr11@gmail.com", "amoh0096@student.monash.edu"]
 
 import openpyxl
 import os
+import argparse
+from sys import platform
 from datetime import datetime, timedelta
 from docxtpl import DocxTemplate
 
@@ -25,23 +27,12 @@ template_file_with_path = 'SAMPLE_WORD.docx'
 excel_filename = 'SAMPLE_EXCEL.xlsx'
 sheets_to_read = ['SHEET1', 'SHEET2']
 quarantine_days_required = 10
+
+# TODO: Implement sqlite which contains the next following info below
 doctors_name = "SAMPLE DOCTOR'S NAME"
 doctors_position = "SAMPLE DOCTOR'S POSITION"
 doctors_appointed_place = "SAMPLE APPOINTED PLACE"
 appointed_place_phone_number = "012-3456789"
-time = '9:30am'
-
-###################################
-# NON-MODIFIABLE GLOBAL VARIABLES #
-###################################
-names = []
-identifications = []
-addresses = []
-phone_numbers = []
-date_hsos = []
-date_ros = []
-source_sheet = []
-todays_date = datetime.today()
 
 ###################################
 #    END OF GLOBAL VARIABLES      #
@@ -53,113 +44,146 @@ todays_date = datetime.today()
 #   UNLESS IF YOU KNOW WHAT YOU   #
 #            ARE DOING            #
 ###################################
-def main():
-    global names, identifications, addresses, phone_numbers, date_hsos, date_ros, source_sheet, sheets_to_read, \
-        todays_date, time, excel_filename
-    excel_file = openpyxl.load_workbook(excel_filename if excel_filename.endswith('.xlsx') else excel_filename + '.xlsx')
+class ExcelParserAndDocxGenerator:
 
-    for sheet in sheets_to_read:
-        parse_data(excel_file, sheet)
-        print(f"[*] Creating folder {sheet}...")
+    # TODO: Add docstring for each method
+
+    def __init__(self, excel_file_name_with_path: str, sheet_names: list[str],
+                 template_file_name_with_path: str = None) -> None:
+        self.excel_file: openpyxl.Workbook = openpyxl.load_workbook(excel_file_name_with_path
+                                                                    if excel_filename.endswith('.xlsx')
+                                                                    else excel_filename + '.xlsx'
+                                                                    )
+        self.sheet_names: list[str] = sheet_names
+        self.patients: dict = {}
+        self.base_directory = os.path.abspath(os.path.dirname(__file__))
+        self.sheet = None
+        self.template_file: DocxTemplate = self.open_and_set_template_file(template_file_name_with_path)
+
+    def open_and_set_template_file(self, template_file_name_with_path: str = None) -> DocxTemplate:
+        if template_file_name_with_path is not None:
+            return DocxTemplate(template_file_name_with_path if template_file_name_with_path.endswith('.docx')
+                                else f"{template_file_name_with_path}.docx")
+        else:
+            return DocxTemplate(self.base_directory + '/SAMPLE_WORD.docx')
+
+    def set_template_file(self, template_file_name_with_path: str = None):
+        self.template_file = self.open_and_set_template_file(template_file_name_with_path)
+
+    def get_available_sheet_names_from_excel_file(self) -> list[str]:
+        return self.excel_file.sheetnames
+
+    def make_directory(self, sheet_name: str) -> str:
+        print(f"[*] Creating folder {sheet_name}...")
         try:
-            os.mkdir(sheet)
-            print(f"[+] Folder {sheet} created successfully!")
+            os.mkdir(f"{self.base_directory}/{sheet_name}")
+            print(f"[+] Folder {sheet_name} created successfully!")
         except FileExistsError:
-            print(f"[-] Folder {sheet} exists.")
+            print(f"[-] Folder {sheet_name} exists.")
+            print(f"[*] Using existing folder...")
 
-    for i in range(len(names)):
-        days_since_first_quarantined = todays_date - datetime.strptime(date_hsos[i], "%d/%m/%Y")
-        if days_since_first_quarantined >= timedelta(days=quarantine_days_required):
-            generate_docx(f"{os.getcwd()}/{sheets_to_read[source_sheet[i]]}/"
-                          f"{i:02}-Borang_RO-{names[i].replace('/', '')}.docx",
-                          names[i], identifications[i], addresses[i], phone_numbers[i], date_hsos[i], date_ros[i])
+        return f"{self.base_directory}\\{sheet_name}" if platform.startswith('win32') \
+            else f"{self.base_directory}/{sheet_name}"
 
+    def read_parse_and_generate_all(self) -> None:
+        self.parse_all()
+        # TODO: Add parsed data to sqlite to keep track of who's letter needs to be generated
+        self.generate_all_docx()
 
-def parse_data(excel_file: openpyxl.Workbook, sheet_name: str) -> None:
-    """
-    This function opens the sheet_name from the opened excel file, and parses
-    - patient's name
-    - patient's identification (MyKad/Passport)
-    - patient's address (maybe employer's address)
-    - patient's phone number (maybe employer's phone number)
-    - patient's first date of quarantine
-    - patient's last date of quarantine
+    def parse_all(self):
+        for sheet_name in self.sheet_names:
+            self.patients[sheet_name] = self.parse_patients_in_sheet(sheet_name)
 
-    :param excel_file: xlsx file to read from
-    :param sheet_name: sheet name from xlsx file to read from
-    """
-    global names, identifications, addresses, phone_numbers, date_hsos, date_ros, source_sheet
-    sheet = excel_file[sheet_name]
+    def parse_patients_in_sheet(self, sheet_name: str) -> list[dict]:
+        self.sheet = self.excel_file[sheet_name]
+        patients: list[dict] = []
 
-    print(f"[*] Parsing data from sheet {sheet_name}...")
-    for row in range(2, sheet.max_row + 1):
-        source_sheet.append(sheets_to_read.index(sheet_name))
+        print(f"[*] Parsing data from sheet {sheet_name}...")
+        # TODO: detect which row contains column headers
+        for row in range(2, self.sheet.max_row + 1):
+            patients.append(self.parse_patient_info(row))
+            print(f"[+] Data from sheet {sheet_name} parsed successfully!")
 
-        name = sheet['B' + str(row)].value
+        return patients
+
+    def parse_patient_info(self, row: int) -> dict:
+        patient_info = {}
+        name = self.sheet['B' + str(row)].value
         if name is None:
-            break
-        names.append(name)
-        print(f"Adding name: {name} from sheet {sheet_name} to record")
+            raise TypeError("[-] Patient has no name.")
 
-        identification_number = sheet['C' + str(row)].value
-        identifications.append(identification_number)
-        print(f"Adding identification number: {identification_number} from sheet {sheet_name} to record")
+        # TODO: Make columns more flexible
+        patient_info["name"] = name
+        print(f"Adding name: {name} to record")
+        identification_number = self.sheet['C' + str(row)].value
+        patient_info["id"] = identification_number
+        print(f"Adding identification number: {identification_number} to record")
+        address = self.sheet['F' + str(row)].value
+        patient_info["address"] = address
+        print(f"Adding work address: {address} to record")
+        phone_number = self.sheet['E' + str(row)].value
+        patient_info["phone_number"] = phone_number
+        print(f"Adding phone number: {phone_number} to record")
+        date_hso = self.sheet['G' + str(row)].value
+        patient_info["date_hso"] = date_hso
+        print(f"Adding date HSO: {date_hso} to record")
+        date_ro = self.sheet['H' + str(row)].value
+        patient_info["date_ro"] = date_ro
+        print(f"Adding date RO: {date_ro} to record")
 
-        address = sheet['F' + str(row)].value
-        addresses.append(address)
-        print(f"Adding work address: {address} from sheet {sheet_name} to record")
+        return patient_info
 
-        phone_number = sheet['E' + str(row)].value
-        phone_numbers.append(phone_number)
-        print(f"Adding phone number: {phone_number} from sheet {sheet_name} to record")
+    def generate_all_docx(self, signed_date_time: datetime = datetime.today()) -> None:
+        # TODO: figure out efficient ways to replace nested for-loops
+        for sheet_name in self.patients.keys():
+            new_directory = self.make_directory(sheet_name)
+            print(f"[*] Generating {new_directory} forms...")
+            for patients in self.patients[sheet_name]:
+                for patient in patients:
+                    self.generate_docx(new_directory, patient, signed_date_time)
+            print(f"[+] Successfully generated {new_directory} forms!")
 
-        date_hso = sheet['G' + str(row)].value
-        date_hsos.append(date_hso)
-        print(f"Adding date HSO: {date_hso} from sheet {sheet_name} to record")
+    def generate_docx(self, path: str, patient_info: dict, signed_date_time: datetime = datetime.today()) -> None:
+        global doctors_name, doctors_position, doctors_appointed_place,\
+            appointed_place_phone_number, quarantine_days_required
 
-        date_ro = sheet['H' + str(row)].value
-        date_ros.append(date_ro)
-        print(f"Adding date RO: {date_ro} from sheet {sheet_name} to record")
-    print(f"[+] Data from sheet {sheet_name} parsed successfully!")
+        days_since_first_quarantined = signed_date_time - datetime.strptime(patient_info["date_hso"], "%d/%m/%Y")
+        if days_since_first_quarantined >= timedelta(days=quarantine_days_required):
+            print(f"[-] {patient_info['name']} still under quarantine...")
+            print(f"[*] Form for {patient_info['name']} not generated.")
+            return
+
+        context = {'name': patient_info["name"].upper(),
+                   'identification_number': patient_info["identification_number"],
+                   'address': patient_info["address"].upper() if patient_info["address"] is not None else "NO GIVEN ADDRESS",
+                   'phone_number': patient_info["phone_number"] if patient_info["phone_number"] is not None else "N/A",
+                   'date_hso': patient_info["date_hso"] if patient_info["date_hso"] is not None else "NO GIVEN DATE",
+                   'date_ro': patient_info["date_ro"] if patient_info["date_ro"] is not None else "NO GIVEN DATE",
+                   'doctors_name': doctors_name.upper(),
+                   'doctors_position': doctors_position.upper(),
+                   'appointed_place': doctors_appointed_place.upper(),
+                   'appointed_place_phone': appointed_place_phone_number,
+                   'todays_date': signed_date_time.strftime('%d/%m/%Y'),
+                   'time': signed_date_time.strftime("%I:%M%p")
+                   }
+
+        self.template_file.render(context)
+        generated_file_name = path if path.endswith('.docx') else f"{path}/{patient_info['name'].replace('/', '')}.docx"
+        print(f"[*] Generating file {generated_file_name}...")
+        self.template_file.save(generated_file_name)
+        print(f"[+] File {generated_file_name} generated successfully!")
 
 
-def generate_docx(generated_name_with_path: str, name: str, identification_number: str, address: str,
-                  phone_number: str, date_hso: str, date_ro: str) -> None:
-    """
-    This function generates a docx file based on the template of Borang RO. Refer to SAMPLE_WORD.docx for an example
-    of the template.
-
-    :param generated_name_with_path: name of the file that is generated
-    :param name: name of patient
-    :param identification_number: MyKad/Passport number of patient
-    :param address: home/work address of patient (most likely employer's address)
-    :param phone_number: phone number of patient/employer
-    :param date_hso: date of first day of quarantine dd/mm/yyyy
-    :param date_ro: date of last day of quarantine dd/mm/yyyy
-    """
-    global template_file_with_path, todays_date, time, doctors_name, doctors_position, \
-        doctors_appointed_place, appointed_place_phone_number
-    doc = DocxTemplate(template_file_with_path if template_file_with_path.endswith('.docx')
-                       else f"{template_file_with_path}.docx")
-
-    context = {'name': name.upper(), 'identification_number': identification_number,
-               'address': address.upper() if address is not None else "NO GIVEN ADDRESS",
-               'phone_number': phone_number if phone_number is not None else "N/A",
-               'date_hso': date_hso if date_hso is not None else "NO GIVEN DATE",
-               'date_ro': date_ro if date_ro is not None else "NO GIVEN DATE",
-               'doctors_name': doctors_name.upper(),
-               'doctors_position': doctors_position.upper(),
-               'appointed_place': doctors_appointed_place.upper(),
-               'appointed_place_phone': appointed_place_phone_number,
-               'todays_date': todays_date.strftime('%d/%m/%Y'),
-               'time': time.upper()
-               }
-
-    doc.render(context)
-    print(f"[*] Generating file {generated_name_with_path}...")
-    doc.save(generated_name_with_path if generated_name_with_path.endswith('.docx')
-             else f"{generated_name_with_path}.docx")
-    print(f"[+] File {generated_name_with_path} generated successfully!")
+def main():
+    global excel_filename, template_file_with_path, sheets_to_read
+    parser = argparse.ArgumentParser(description="read excel spreadsheet and/or generate RO form", add_help=True)
+    parser.add_argument("-v", "--verbose", help="print out messages (helpful when debugging)")
+    parser.add_argument("-s", "--source-file", help="specify source file to parse (.xlsx format)")
+    parser.add_argument("-t", "--template-file", help="specify template file to generate from (.docx format)")
+    # TODO: complete options and implement them into app instead of using default global variables' values
+    args = parser.parse_args()
+    app = ExcelParserAndDocxGenerator(excel_filename, sheets_to_read, template_file_with_path)
+    app.generate_all_docx()
 
 
 if __name__ == '__main__':
